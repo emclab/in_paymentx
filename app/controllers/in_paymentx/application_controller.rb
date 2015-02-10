4dedd82118390065e970e3de0bb08f5ed16e73ee
jc <emclab2011@gmail.com>
2015-02-10 13:51:04 -0600

    JC:added return customers/projects by access right to application contorller for search.  more cn. spec passes.

--- app/controllers/in_paymentx/application_controller.rb
+++ app/controllers/in_paymentx/application_controller.rb
@@ -23,5 +23,18 @@ module InPaymentx
     def view_in_config?
       @view_in_config = Authentify::AuthentifyUtility.load_view_in_config
     end
+    
+    #engine's
+    def return_customers_by_access_right     
+      access_rights, model_ar_r, has_record_access = access_right_finder('index', InPaymentx.customer_class.to_s.underscore.pluralize)
+      return [] if access_rights.blank?
+      return model_ar_r #instance_eval(access_rights.sql_code) #.present?
+    end
+    
+    def return_projects_by_access_right     
+      access_rights, model_ar_r, has_record_access = access_right_finder('index', InPaymentx.project_class.to_s.underscore.pluralize)
+      return [] if access_rights.blank?
+      return model_ar_r #instance_eval(access_rights.sql_code) #.present?
+    end
   end
 end
--- lib/in_paymentx/version.rb
+++ lib/in_paymentx/version.rb
@@ -1,3 +1,3 @@
 module InPaymentx
-  VERSION = "3.2.12.07"
+  VERSION = "3.2.12.08"
 end
