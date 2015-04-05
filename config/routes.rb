1af6703bba5de142599036c4e86db8e2f530f192
jc <emclab2011@gmail.com>
2015-04-04 21:48:36 -0500

    JC: added search/stats to routes.rb. spec passes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 InPaymentx::Engine.routes.draw do
   
-  resources :payments
+  resources :payments do
+    collection do
+      get :search
+      get :search_results
+      get :stats
+      get :stats_results 
+    end
+  end
   
   root :to => 'payments#index'
 end
